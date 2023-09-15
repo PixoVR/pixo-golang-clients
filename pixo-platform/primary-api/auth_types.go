1aa9802ce80886f5e3b6c092ee0ba527f5b45c59
Walker O'Brien <walker.obrien@pixovr.com>
2023-09-15 16:08:16 -0500

    organize types

---
+++ pixo-platform/primary-api/auth_types.go
@@ -0,0 +1,21 @@
+package primary_api
+
+type LoginRequest struct {
+	Login    string `json:"login"`
+	Password string `json:"password"`
+}
+
+type AuthResponse struct {
+	User struct {
+		Token string `json:"authToken"`
+		Role  string `json:"role"`
+	}
+}
+
+type User struct {
+	ID         int    `json:"id"`
+	First      string `json:"first"`
+	Last       string `json:"last"`
+	Email      string `json:"email"`
+	ExternalID string `json:"externalId"`
+}
---
+++ pixo-platform/primary-api/multiplayer_types.go
@@ -0,0 +1,33 @@
+package primary_api
+
+import "time"
+
+type MultiplayerServerConfig struct {
+	ID       int   `json:"id"`
+	ModuleID int   `json:"moduleId"`
+	Capacity int32 `json:"capacity"`
+	Disabled bool  `json:"disabled"`
+
+	CreatedBy string `json:"createdBy"`
+	UpdatedBy string `json:"updatedBy"`
+
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
+}
+
+type MultiplayerServerVersion struct {
+	ModuleID         int    `json:"moduleId,omitempty"`
+	VersionID        int    `json:"versionId,omitempty"`
+	Engine           string `json:"engine,omitempty"`
+	Status           string `json:"status,omitempty"`
+	ImageRegistry    string `json:"imageRegistry"`
+	Version          string `json:"version,omitempty"`
+	MinClientVersion string `json:"minClientVersion,omitempty"`
+	Filename         string `json:"filename,omitempty"`
+
+	CreatedBy string `json:"createdBy"`
+	UpdatedBy string `json:"updatedBy"`
+
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
+}
