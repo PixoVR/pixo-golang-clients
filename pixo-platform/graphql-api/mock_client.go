402defe7a61f7605e2b3a56e2ecd0ca4dab53331
Walker O'Brien <walker.obrien@pixovr.com>
2024-01-24 01:01:45 -0600

    add fake key

--- pixo-platform/graphql-api/mock_client.go
+++ pixo-platform/graphql-api/mock_client.go
@@ -142,6 +142,7 @@ func (m *MockGraphQLClient) CreateAPIKey(ctx context.Context, input platform.API
 	}
 
 	input.ID = 1
+	input.Key = faker.UUIDHyphenated()
 	input.CreatedAt = time.Now().UTC()
 	input.UpdatedAt = time.Now().UTC()
 
